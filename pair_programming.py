58255a4fbb1500d3a3a469434bfbd338a26f671f
Shelby Layne <shelby_layne@mines.edu>
2026-03-31 13:15:35 -0600

    Add notebook ready for feedback from @Addy

---
+++ pair_programming.py
@@ -0,0 +1,19 @@
+# import math function to use sin and cos
+import math
+
+# Functions takes in given polar coordinates (r and theta) and outputs cartesian coordinates (x and y)
+def polar_to_cartesian(r, theta):
+    x = r*math.cos(theta)
+    y = r*math.sin(theta)
+    return x,y
+
+# Where to input r and theta
+r = float(input("Enter radius (r): "))
+theta = float(input("Enter angle in radians (theta): "))
+
+x,y = polar_to_cartesian(r, theta)
+
+# Prints answer to 2 decimal points
+print("Cartesian coordinates:")
+print(f"x = {x: .2f}")
+print(f"y = {y: .2f}")
